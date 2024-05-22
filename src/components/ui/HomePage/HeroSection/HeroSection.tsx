15a1229bb12d2142cfc3327ccded79c01957531d
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-05-22 16:41:39 +0600

    hero-seciton-responsive-modify

--- src/components/ui/HomePage/HeroSection/HeroSection.tsx
+++ src/components/ui/HomePage/HeroSection/HeroSection.tsx
@@ -27,17 +27,31 @@ const HeroSection = () => {
         navigation={true}
         loop={true} // Enable looping for smooth transitions
         modules={[Autoplay, Pagination, Navigation]}
-        speed={2000} // Set the transition speed to 1000ms (1 second)
+        speed={1500} // Set the transition speed to 1000ms (1 second)
         className="mySwiper"
       >
         <SwiperSlide>
           <div className="header ">
             <div className="header-content capitalize">
-              <div className="left-side">
+            <div className="left-side">
                 <h1 className="welcome-heading text-sm md:text-2xl lg:text-4xl font-bold text-[#0685FF]">
                 Education is the passport to the future, 
                 </h1>
-                <p className="mt-3 text-[10px] md:text-xl font-semibold welcome-paragraph ">
+                <p className=" text-sm md:text-xl font-semibold welcome-paragraph ">
+                for tomorrow belongs to those who prepare for it today.
+                </p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="header header2">
+            <div className="header-content capitalize">
+            <div className="left-side">
+                <h1 className="welcome-heading text-sm md:text-2xl lg:text-4xl font-bold text-[#0685FF]">
+                Education is the passport to the future, 
+                </h1>
+                <p className=" text-sm md:text-xl font-semibold welcome-paragraph ">
                 for tomorrow belongs to those who prepare for it today.
                 </p>
               </div>
@@ -46,33 +60,20 @@ const HeroSection = () => {
         </SwiperSlide>
 
         <SwiperSlide>
-          <div className="header header2">
+        <div className="header header3">
             <div className="header-content capitalize">
             <div className="left-side">
                 <h1 className="welcome-heading text-sm md:text-2xl lg:text-4xl font-bold text-[#0685FF]">
                 Education is the passport to the future, 
                 </h1>
-                <p className="mt-3 text-sm md:text-xl font-semibold welcome-paragraph ">
+                <p className=" text-sm md:text-xl font-semibold welcome-paragraph ">
                 for tomorrow belongs to those who prepare for it today.
                 </p>
               </div>
             </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide>
-          <div className="header header2">
-            <div className="header-content capitalize">
-            <div className="left-side">
-                <h1 className="welcome-heading text-sm md:text-2xl lg:text-4xl font-bold text-[#0685FF]">
-                Education is the passport to the future, 
-                </h1>
-                <p className="mt-3 text-sm md:text-xl font-semibold welcome-paragraph ">
-                for tomorrow belongs to those who prepare for it today.
-                </p>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
+        
         {/* Add more SwiperSlide components as needed */}
       </Swiper>
     </div>
