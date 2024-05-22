cec07d5b1cb055e3c489f9d00987aa8a48ed2644
AlMamunTalukder <almamuntalukder0@gmail.com>
2024-05-22 18:29:47 +0600

    responsive

--- src/app/(mainlayout)/club/computer/page.tsx
+++ src/app/(mainlayout)/club/computer/page.tsx
@@ -22,20 +22,20 @@ const computer = () => {
                                 </h3>
                             </div>
                             {/* Divider  */}
-              <div className="w-full  border-t border-gray-300 h-[.5px] my-3 md:my-6 lg:my-6"></div>
-                            <section className="lg:w-auto border rounded p-4 bg-white text-gray-800">
+                                <div className="w-full  border-t border-gray-300 h-[.5px] my-3 md:my-6 lg:my-6"></div>
+                                <section className="lg:w-auto border rounded p-1 md:p-2 lg:p-4 bg-white text-gray-800">
                                 <Image src={logo} className="h-[200px] w-[200px] mx-auto" alt="this is logo" />
                                 <section className="   text-gray-800">
-                                    <div className="container max-w-5xl px-4 py-12 mx-auto">
-                                        <div className="grid gap-4 mx-4 sm:grid-cols-12">
+                                    <div className="container max-w-5xl px-1 md:px-2 lg:px-4 py-12 mx-auto">
+                                        <div className="grid gap-4 mx-1 md:mx-2 lg:mx-4 sm:grid-cols-12">
                                             <div className="col-span-12 sm:col-span-4">
                                                 <div className="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:bg-violet-600">
                                                     <h3 className="text-3xl font-semibold">Computer Club</h3>
                                                     <span className="text-sm font-bold tracking-wider uppercase   text-gray-600">Since 2001</span>
                                                 </div>
                                             </div>
-                                            <div className="relative col-span-12 px-4 space-y-6 sm:col-span-7">
-                                                <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-300">
+                                            <div className="relative col-span-12 px-1 md:px-2 lg:px-4 space-y-6 sm:col-span-7 text-justify">
+                                                <div className="col-span-12 space-y-12 relative px-1 md:px-2 lg:px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-300">
                                                     <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-violet-600">
                                                         <h3 className="text-xl font-semibold tracking-wide">কম্পিউটার ক্লাব প্রতিষ্ঠা</h3>
                                                         <time className="text-xs tracking-wide uppercase   text-gray-600">জানুয়ারী ২০১০</time>
